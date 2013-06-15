429ccb58282e2857fb3373eddcbe64084c1d7e51
Edu <ew@omikron.net>
2013-06-15 21:22:29 +0200

    fluid change between light program steps

--- lightmashine/SimPwmLed.cpp
+++ lightmashine/SimPwmLed.cpp
@@ -3,16 +3,45 @@
 SimPwmLed::SimPwmLed(byte pin) {
    _pin = pin;
    pinMode(pin, OUTPUT);
+   
+   _updatesPerTargetLightnessPoint = 0;
+    
+   _fromTargetLightness = NOT_SET;
+   _toTargetLightness = NOT_SET;
+    
    _counter = 0;
+   _updatesBetweenFrames = 0;
+   _updatesPerTargetLightnessPoint = 0;
+   _updatesUntilNextTargetLightPoint = 0;
    _targetLightness = 0;
-   set(0);
-   update();
 }
 
 void SimPwmLed::set(byte percent) {
   if (percent > 100) percent = 100;
 
-  _targetLightness = percent;
+  _fromTargetLightness = _toTargetLightness;
+  _toTargetLightness = percent;
+
+  if (_updatesBetweenFrames > 0) {
+    int diff;
+    if (_toTargetLightness > _fromTargetLightness) {
+      diff = _toTargetLightness - _fromTargetLightness;
+      _increasing = true;
+    } else {
+      diff = _fromTargetLightness - _toTargetLightness;
+      _increasing = false;
+    }
+    if (diff == 0) {diff = 1;} // avoid division by zero
+    _updatesPerTargetLightnessPoint = _updatesBetweenFrames / diff;
+  }
+
+  if (_fromTargetLightness == NOT_SET) {
+    // very first run
+    _targetLightness = percent;
+  } else {
+    _targetLightness = _fromTargetLightness;
+  }
+  _updatesBetweenFrames = 0;
 }
 
 void SimPwmLed::update() {
@@ -23,11 +52,26 @@ void SimPwmLed::update() {
   }
   _counter++;
   if (_counter == 100) _counter = 0;
+
+  _updatesUntilNextTargetLightPoint--;
+  if (_updatesUntilNextTargetLightPoint <= 0) {
+    _updatesUntilNextTargetLightPoint = _updatesPerTargetLightnessPoint;
+
+    if (_increasing) {
+      _targetLightness++;
+      if (_targetLightness > _toTargetLightness) _targetLightness = _toTargetLightness;
+    } else {
+      _targetLightness--;
+      if (_targetLightness < _toTargetLightness) _targetLightness = _toTargetLightness;
+    }
+  }
+
+  _updatesBetweenFrames++;
 }
 
 void SimPwmLed::fastWrite(byte newState) {
   if (_state != newState) {
-    digitalWrite(_pin, newState);
-    _state = newState;
+      digitalWrite(_pin, newState);
+     _state = newState;
   }
 }
--- lightmashine/SimPwmLed.h
+++ lightmashine/SimPwmLed.h
@@ -4,6 +4,8 @@
 #include "Arduino.h"
 #include "Led.h"
 
+#define NOT_SET 255
+
 class SimPwmLed: public Led {
   public:
     SimPwmLed(byte pin);
@@ -13,7 +15,15 @@ class SimPwmLed: public Led {
     void fastWrite(byte newState);
     
     byte _pin;
-    byte _targetLightness;
+    int _updatesBetweenFrames;
+    int _updatesPerTargetLightnessPoint;
+    int _updatesUntilNextTargetLightPoint;
+    bool _increasing;
+
+    byte _fromTargetLightness;
+    byte _toTargetLightness;
+
+    int _targetLightness;
     byte _counter;
     byte _state;
 };
