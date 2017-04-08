3e36721a6738b3525bd24187aba94734ba5afcab
Eduard Wirch <ew@omikron.net>
2017-04-08 22:58:17 +0200

    Increase control signal accuracy
    Since the signal duration is measured in the main loop, it is affected by the loop run duration. This can vary significantly if the program switches LED program frames. But this happens only once and then. So the strategy is to discard control signals if they were measured during a heavy main loop iteration.

--- lightmashine/RecieverChannel.cpp
+++ lightmashine/RecieverChannel.cpp
@@ -7,6 +7,7 @@ RecieverChannel::RecieverChannel(int pin, int minSignal, int maxSignal) {
   _value = 0;
   _lastState = LOW;
   _stateChanged = micros();
+  _dropNextValue = false;
 }
 
 void RecieverChannel::read() {
@@ -22,14 +23,18 @@ void RecieverChannel::read() {
       if (time > MIN_VALUE) {
         _value = trimValueToBoundaries(time);
       }
+      if (_dropNextValue) {
+        _value = 0;
+        _dropNextValue = false;
+      }
     }
     _lastState = newState;
   }
-}	
+}
 
 int RecieverChannel::getValue() {
   return _value;
-}	
+}
 
 int RecieverChannel::trimValueToBoundaries(long val) {
   if (val < _minSignal) {
@@ -48,3 +53,9 @@ int RecieverChannel::getMaxSignal() {
 int RecieverChannel::getMinSignal() {
   return _minSignal;
 }
+
+void RecieverChannel::dropNextValue() {
+  _dropNextValue = true;
+}
+
+
